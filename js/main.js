8ef0e5f6d514e5f9bff699fe4a16113e929839f5
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-18 09:11:16 -0400

    fixed js and css

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $('html').on('keydown', function (e) {
   var $name = $('<div>');
   $name.addClass('ball');
   $body.append($name);
+  var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
+  var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
+  $name.css('left', newLeft);
+  $name.css('top', newTop);
 });
